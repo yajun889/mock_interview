a44eaeaa8e598368c2cb48c34cff25744597720d
yajun889 <yajun889@gmail.com>
2020-11-04 21:34:10 -0800

    add all project files

---
+++ editor/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home_page, name="home_page"),
+    path('ajax/result', views.result, name="result")
+]
